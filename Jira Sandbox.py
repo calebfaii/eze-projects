a3291af15e58e786ce311744fc25ee25b47fe3d8
Caleb Fall <caleb.a.fall@gmail.com>
2016-12-20 17:34:41 -0500

    V3

--- Jira Sandbox.py
+++ Jira Sandbox.py
@@ -7,6 +7,12 @@ link = secrets.website
 username = secrets.username
 pw = secrets.password
 
+print " "
+print "Welcome to Epic Checker."
+print ' '
+print "_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+print ' '
+
 
 def connectJira(jira_server, jira_user, jira_password):
     """"
@@ -14,12 +20,15 @@ def connectJira(jira_server, jira_user, jira_password):
     """
     try:
         print "Connecting to JIRA: %s" % jira_server
+        print " "
         jira_options = {'server': jira_server}
         jira = JIRA(options=jira_options,
                     basic_auth=(jira_user,
                                 jira_password))
         print "JIRA connection successful."
-        print " "
+        print ' '
+        print "_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+        print ' '
         return jira
     except Exception, e:
         print "Failed to connect to JIRA: %s" % e
@@ -34,6 +43,7 @@ def getLaceEpics():
     url = "https://lace.ezesoft.net/api/releases/reports/product-team/100"
     try:
         print "Retrieving Epics from LACE..."
+        print ' '
         response = urllib.urlopen(url)
         root = json.loads(response.read())
         prod = root["Products"]
@@ -46,7 +56,9 @@ def getLaceEpics():
                     epic_team_jira = (epicname, teamname, jirakey)
                     lace_epics.append(epic_team_jira)
         print "Epics returned successfully."
-        print " "
+        print ' '
+        print "_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+        print ' '
         return lace_epics
     except Exception, e:
         print "Failed to retrieve Epics: %s" % e
@@ -59,11 +71,17 @@ def getJiraEpics():
     jira_epics = []
     jira_epic_names = []
     jira = connectJira(link, username, pw)
-    issues_in_version = jira.search_issues("fixVersion='5.7 SR10.12.0' AND type != Epic", maxResults=500)
+    print "Performing lookup..."
+    print " "
+    print "[*   ]"
+    issues_in_version = jira.search_issues("fixVersion='5.7 SR10.12.0' AND type != Epic",
+                                           maxResults=500)
+    print "[**  ]"
     for issue in issues_in_version:
         epiclink = issue.fields.customfield_10860
         if epiclink not in jira_epics and type(epiclink) != None:
             jira_epics.append(epiclink)
+    print "[*** ]"
     for epic in jira_epics:
         try:
             e = jira.issue(epic)
@@ -72,17 +90,25 @@ def getJiraEpics():
             jira_epic_names.append(epic_key)
         except:
             continue
+    print "[****]"
     return jira_epic_names
 
 lace_epics = getLaceEpics()
 jepics = getJiraEpics()
 
 lace_keys = []
-for item in lace_epics:
-    lkey = item[2]
+for epic in lace_epics:
+    lkey = epic[2]
     lace_keys.append(lkey)
 
+print " "
+print "UNLINKED EPICS:"
+print "_______________"
+print " "
 for jepic in jepics:
     ekey = jepic[1]
     if ekey not in lace_keys:
-        print "UNLINKED: Team = ", str(jepic[0]), "Key = ", str(jepic[1])
+        print "Epic: ", str(jepic[0])
+        print "Key: ", str(jepic[1])
+        print " "
+print "< End of list. >"
