6b24c56d90ed30261d166f568814cce25c7408ed
Caleb Fall <cfall@ezesoft.com>
2017-10-17 09:19:10 -0400

    Version 1.1 - not stable

--- hf/omshf.py
+++ hf/omshf.py
@@ -34,86 +34,160 @@ class JiraSession(object):
     user = secrets.user
     pw = secrets.pw
     options = {'server': 'https://jira.ezesoft.net'}
-    session = None
+    hotfixJiraIDs = None
+    JIRAObject = None
+    hotfixApprover = None
     hfJiraID = None
-    hotfix = None
-    mainline = None
+    hotfixJiraIssueObjects = []
+    mainlineJiraID = None
+    mainlineJiraIssueObject = None
+    hotfixSummary = None
     __printer = Printer()
 
-    def __init__(self, hfJiraID):
+    def __init__(self, hotfixJiraIDList):
 
-        self.hfJiraID = hfJiraID
+        self.hotfixJiraIDs = hotfixJiraIDList
         self.connectToJira()
 
     @staticmethod
-    def makeIssue():
+    def makeIssue(jiraID):
 
-        return JiraIssue()
+        return JiraIssue(jiraID)
 
     def connectToJira(self):
 
         try:
-            self.session = JIRA(basic_auth=(self.user, self.pw), options=self.options)
+            self.JIRAObject = JIRA(basic_auth=(self.user, self.pw), options=self.options)
             self.__printer.pPrint('S', 'JIRA connection successful.')
         except Exception, e:
             self.__printer.pPrint('E', 'JIRA connection failed: %s' % e)
             raise StandardError
 
-    def setHotfix(self, jiraIssue):
+    def populateHotfixObjectList(self, jiraIssueObject):
 
-        self.hotfix = jiraIssue
+        self.hotfixJiraIssueObjects.append(jiraIssueObject)
 
-    def setMainline(self, jiraIssue):
+    def findMLIssueKeyInIssueLinks(self, jiraIssueObject):
 
-        self.mainline = jiraIssue
+        issueLinks = jiraIssueObject.raw['fields']['issuelinks']
+        for issue in issueLinks:
+            try:
+                key = issue['inwardIssue']['key']
+            except Exception, e:
+                self.__printer.pPrint("E", "Inward issue link not found: %s" % e)
+                self.__printer.pPrint("E", "Locating outward issues...")
+                key = issue['outwardIssue']['key']
+                if key:
+                    self.__printer.pPrint("S", 'Mainline issue located.')
+                else:
+                    self.__printer.pPrint("S", 'Mainline issue not found.')
+            jiraIssue = self.JIRAObject.issue(key)
+            issueSummary = jiraIssue.fields.summary
+            if issueSummary[0:4] == 'MAIN':
+                if key[0:3] != 'CRS' and key[0:3] != 'DOC':
+                    return key
+        return None
+
+    def setMainlineJiraID(self, jiraIssueID):
+
+        if not self.mainlineJiraID:
+            self.mainlineJiraID = str(jiraIssueID)
+
+    def setMainlineIssueObject(self, jiraIssueObject):
+
+        if not self.mainlineJiraIssueObject:
+            self.mainlineJiraIssueObject = jiraIssueObject
+
+    def getJiraProjectKeyFromJira(self):
+
+        jiraIssueObject = self.hotfixJiraIssueObjects[0]
+        return jiraIssueObject.jiraObject.raw['fields']['project']['key']
+
+    def getProjectLeadFromJira(self):
+
+        projectName = self.getJiraProjectKeyFromJira()
+        projectObject = self.JIRAObject.project(projectName)
+        return projectObject.lead.displayName
+
+    def setJiraApproverNameFromJira(self):
+
+        approverFullName = [str(word) for word in self.getProjectLeadFromJira().split(" ")]
+        approverLastName = approverFullName[-1]
+        self.hotfixApprover = approverLastName
 
     def generateContent(self):
 
-        hotfixJiraObject = self.makeIssue()
-        hotfixJiraObject.populateBasic(self, self.hfJiraID)
-        hotfixJiraObject.populateAll(self)
-        self.setHotfix(hotfixJiraObject)
+        hotfixJiraObjects = []
+        for i in self.hotfixJiraIDs:
+            issueObj = self.makeIssue(i)
+            hotfixJiraObjects.append(issueObj)
+        for issue in hotfixJiraObjects:
+            issue.populateJiraFieldAttributes(self)
+            issue.populateSalesforceFieldAttributes()
+            mainlineIssueKey = self.findMLIssueKeyInIssueLinks(issue.jiraObject)
+            self.setMainlineJiraID(mainlineIssueKey)
+        for i in hotfixJiraObjects:
+            self.populateHotfixObjectList(i)
 
-        mainlineJiraObject = self.makeIssue()
-        mainlineJiraObject.populateBasic(self, str(hotfixJiraObject.mainlineIssue))
-        self.setMainline(mainlineJiraObject)
+        mainlineJiraObject = self.makeIssue(self.mainlineJiraID)
+        mainlineJiraObject.populateJiraFieldAttributes(self)
+        self.setMainlineIssueObject(mainlineJiraObject)
+        self.setJiraApproverNameFromJira()
+        self.setHotfixSummary()
 
     def getMLCaseStatus(self):
 
-        if self.mainline.status == 'Closed':
-            return "This issue has been resolved in the head version in %s with " % self.mainline.fixVersion
+        if self.mainlineJiraIssueObject.status == 'Closed':
+            return "This issue has been resolved in the head version in %s with " % \
+                   self.mainlineJiraIssueObject.fixVersion
         else:
-            return "This issue will be resolved in the head version (%s) in " % self.mainline.fixVersion
+            return "This issue will be resolved in the head version (%s) in " % \
+                   self.mainlineJiraIssueObject.fixVersion
 
-    def createEmailSubject(self):
+    def setHotfixSummary(self):
 
-        summary = self.hotfix.summary
+        summary = self.hotfixJiraIssueObjects[0].summary
         if summary[0:2] == 'SR':
             firstHyphen = summary.find("-") + 2
             if firstHyphen:
-                return summary[firstHyphen:]
+                self.hotfixSummary = summary[firstHyphen:]
+                return
         else:
-            return summary
+            self.hotfixSummary = summary
+
+    def generateIssueDict(self, jiraIssueObject):
+
+        issueDict = {'clientInfo': {'clientName': jiraIssueObject.client,
+                                    'clientLink': jiraIssueObject.clientLink},
+                     'dateIn': jiraIssueObject.dateIn,
+                     'sourceInfo': {'sourceID': jiraIssueObject.sourceID,
+                                    'sourceLink': jiraIssueObject.sourceLink},
+                     'sfInfo': {'sfCaseNum': jiraIssueObject.salesforceCaseNumber,
+                                'sfCaseLink': jiraIssueObject.salesforceCaseLink},
+                     'category': jiraIssueObject.category,
+                     'version': jiraIssueObject.fixVersion}
+        return issueDict
+
+    def generateAllIssueDictList(self):
+
+        dictList = []
+        for issue in self.hotfixJiraIssueObjects:
+            key = issue.sourceID
+            value = self.generateIssueDict(issue)
+            dictList.append({key: value})
+        return dictList
 
     def generateAnnouncement(self):
 
-        announcementContent = {'emailSubject': 'HOTFIX ANNOUNCEMENT: %s' % self.createEmailSubject(),
+        dictList = self.generateAllIssueDictList()
+        announcementContent = {'emailSubject': 'HOTFIX ANNOUNCEMENT: %s' % self.hotfixSummary,
                                'approvalInfo': "Hotfix approved by %s. %s." %
-                                               (self.hotfix.approver,
-                                                self.hotfix.summary),
+                                               (self.hotfixApprover,
+                                                self.hotfixSummary),
                                'mainlineInfo': self.getMLCaseStatus(),
-                               'mainlineLink': self.mainline.sourceLink,
-                               'mainlineID': self.mainline.sourceID,
-                               'client': self.hotfix.client,
-                               'clientLink': self.hotfix.clientLink,
-                               'dateIn': self.hotfix.dateIn,
-                               'sourceID': self.hotfix.sourceID,
-                               'sourceLink': self.hotfix.sourceLink,
-                               'sfCaseNum': self.hotfix.salesforceCaseNumber,
-                               'sfCaseLink': self.hotfix.salesforceCaseLink,
-                               'category': self.hotfix.category,
-                               'version': self.hotfix.fixVersion}
-
+                               'mainlineLink': self.mainlineJiraIssueObject.sourceLink,
+                               'mainlineID': self.mainlineJiraIssueObject.sourceID,
+                               'hotfixes': dictList}
         return announcementContent
 
 
@@ -135,23 +209,24 @@ class JiraIssue(object):
     mainlineIssue = None
     approver = None
 
-    def populateBasic(self, jiraSession, jiraIssue):
+    def __init__(self, jiraID):
 
-        self.jiraObject = jiraSession.session.issue(jiraIssue)
+        self.sourceID = jiraID
+
+    def populateJiraFieldAttributes(self, jiraSession):
+
+        self.jiraObject = jiraSession.JIRAObject.issue(self.sourceID)
         self.summary = self.jiraObject.fields.summary
-        self.fixVersion = self.jiraObject.fields.fixVersions[0]
-        self.sourceID = jiraIssue
+        self.fixVersion = str(self.jiraObject.fields.fixVersions[0])
         self.sourceLink = '%s/browse/%s' % (jiraSession.options['server'], self.sourceID)
         self.status = str(self.jiraObject.raw['fields']['status']['name'])
 
-    def populateAll(self, jiraSession):
+    def populateSalesforceFieldAttributes(self):
 
         self.dateIn = self.formatDate()
-        self.setSFCaseAndLink()
-        self.setCategory()
-        self.setClientNameAndLink()
-        self.findMLCase(jiraSession)
-        self.setApprover(jiraSession)
+        self.populateSalesforceCaseAndLink()
+        self.populateCategory()
+        self.populateClientNameAndLink()
 
     def formatDate(self):
 
@@ -166,7 +241,7 @@ class JiraIssue(object):
 
         return 'https://ezesoft.my.salesforce.com/%s' % link
 
-    def getSFCaseNum(self):
+    def getSFCaseNumFromJira(self):
 
         try:
             caseNum = self.jiraObject.raw['fields']['customfield_11561']
@@ -175,7 +250,7 @@ class JiraIssue(object):
             caseNum = "-"
         return caseNum
 
-    def getSFCaseLink(self):
+    def getSFCaseLinkFromJira(self):
 
         try:
             caseLink = self.formatSFLink(self.jiraObject.raw['fields']['customfield_12863'][0])
@@ -184,12 +259,12 @@ class JiraIssue(object):
             caseLink = None
         return caseLink
 
-    def setSFCaseAndLink(self):
+    def populateSalesforceCaseAndLink(self):
 
-        self.salesforceCaseNumber = self.getSFCaseNum()
-        self.salesforceCaseLink = self.getSFCaseLink()
+        self.salesforceCaseNumber = self.getSFCaseNumFromJira()
+        self.salesforceCaseLink = self.getSFCaseLinkFromJira()
 
-    def getClientName(self):
+    def getClientNameFromJira(self):
 
         try:
             clientName = self.jiraObject.raw['fields']['customfield_12969']
@@ -198,7 +273,7 @@ class JiraIssue(object):
             clientName = 'None'
         return clientName
 
-    def getClientLink(self):
+    def getClientLinkFromJira(self):
 
         try:
             clientLink = self.formatSFLink(self.jiraObject.raw['fields']['customfield_12861'][0])
@@ -207,12 +282,12 @@ class JiraIssue(object):
             clientLink = None
         return clientLink
 
-    def setClientNameAndLink(self):
+    def populateClientNameAndLink(self):
 
-        self.client = self.getClientName()
-        self.clientLink = self.getClientLink()
+        self.client = self.getClientNameFromJira()
+        self.clientLink = self.getClientLinkFromJira()
 
-    def setCategory(self):
+    def populateCategory(self):
 
         # TODO: investigate this method with some more use cases.
 
@@ -237,53 +312,24 @@ class JiraIssue(object):
                 return
             self.category = category
 
-    def findMLCase(self, jiraSession):
+hotfixJiraIDs = []
+while True:
+    hfJiraID = raw_input("Hotfix JIRA ID: ")
+    if hfJiraID:
+        hotfixJiraIDs.append(hfJiraID)
+    if not hfJiraID:
+        break
 
-        issueLinks = self.jiraObject.raw['fields']['issuelinks']
-        for issue in issueLinks:
-            try:
-                key = issue['inwardIssue']['key']
-            except Exception, e:
-                self.__printer.pPrint("E", "Inward issue link not found: %s" % e)
-                self.__printer.pPrint("E", "Locating outward issues...")
-                key = issue['outwardIssue']['key']
-                if key:
-                    self.__printer.pPrint("S", 'Mainline issue located.')
-                else:
-                    self.__printer.pPrint("S", 'Mainline issue not found.')
-            jiraIssue = jiraSession.session.issue(key)
-            issueSummary = jiraIssue.fields.summary
-            if issueSummary[0:4] == 'MAIN':
-                if key[0:3] != 'CRS' and key[0:3] != 'DOC':
-                    self.mainlineIssue = key
-        return
+# session = JiraSession(hotfixJiraIDs)
+# session.generateContent()
+# cont = session.generateAnnouncement()
+# print "OK"
 
-    def getJiraProjectKey(self):
-
-        return self.jiraObject.raw['fields']['project']['key']
-
-    def getProjectLead(self, jiraSession):
-
-        projectName = self.getJiraProjectKey()
-        projectObject = jiraSession.session.project(projectName)
-        return projectObject.lead.displayName
-
-    def getJiraApproverName(self, jiraSession):
-
-        approverFullName = [str(word) for word in self.getProjectLead(jiraSession).split(" ")]
-        approverLastName = approverFullName[-1]
-        return approverLastName
-
-    def setApprover(self, jiraSession):
-
-        self.approver = self.getJiraApproverName(jiraSession)
-
-hfJiraID = raw_input("Hotfix JIRA ID: ")
 app = Flask(__name__)
 
 @app.route('/')
-def go(hfJiraID=hfJiraID):
-    session = JiraSession(hfJiraID)
+def go(hotfixJiraIDList=hotfixJiraIDs):
+    session = JiraSession(hotfixJiraIDs)
     session.generateContent()
     content = session.generateAnnouncement()
     return render_template('index.html', **content)
@@ -291,5 +337,5 @@ webbrowser.open("localhost:5000/", new=1, autoraise=True)
 if __name__ == "__main__":
     app.run()
 
-# TODO: Test cases
-# TODO: Support multiple child hotfix tickets
+# CREATE A DICT OF 'KEYS' - EACH KEY IS A HF ID, THE VALUE IS THE CONTENT DICT
+# PASS THE KEYS TO THE APP BY CREATING A METHOD
